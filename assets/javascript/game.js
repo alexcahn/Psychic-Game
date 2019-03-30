fa624eff1c00a9500755a67af27079ff8cc301a0
Alex Cahn <alexbcahn@gmail.com>
2019-03-30 09:48:36 -0500

    committing final project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,8 +13,10 @@ var WinsText = document.getElementById("wins-text");
 var LossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guesses-left");
 var yourGuess = document.getElementById("user-choices");
+// Randomly chooses a choice from the lettersGuessed array.
+var psychicLetter = lettersGuessed[Math.floor(Math.random() * lettersGuessed.length)];
+console.log(psychicLetter)
 
-    
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
@@ -24,47 +26,47 @@ document.onkeyup = function (event) {
     console.log(userGuess);
 
     // function to restart the game
-    function restart(){
-        if ((userGuess === psychicLetter) || 
+    function restart() {
+        if ((userGuess === psychicLetter) ||
             (guessesLeft === 0));
-            guessesLeft = 9;
-            guessesSoFar = [];
+        guessesLeft = 9;
+        guessesSoFar = [];
+        var psychicLetter = lettersGuessed[Math.floor(Math.random() * lettersGuessed.length)];
+        console.log(psychicLetter);
     }
-
-
-    // Randomly chooses a choice from the lettersGuessed array.
-    var psychicLetter = lettersGuessed[Math.floor(Math.random() * lettersGuessed.length)];
 
     // if you guess a letter you've already guessed, it tell you that you've guessed it
-if(lettersGuessed.indexOf(userGuess) === -1){
-    alert ("that's not a letter!");
-    return false;
-}
-
-    if (guessesSoFar.indexOf(userGuess) >=0){
+    // if you guess a letter in the -1(not in the lettersGuessed array) there's an alert
+    if (lettersGuessed.indexOf(userGuess) === -1) {
+        alert("that's not a letter!");
+        return false;
+    }
+    // if your guess is already in the index of your guesses so far
+    if (guessesSoFar.indexOf(userGuess) >= 0) {
         alert("you already guessed that!");
+        // if it's not a letter you've chosen, your guess will be pushed into the array of your guesses and you'll get -- guess left
     } else {
         guessesSoFar.push(userGuess);
-        guessesLeft --;
+        guessesLeft--;
     }
-//   if you guess the right letter
-    if (userGuess === psychicLetter){
+    //   if you guess the right letter
+    if (userGuess === psychicLetter) {
         Wins++;
         alert("you win!");
         guessesLeft = 9;
         guessesSoFar = [];
         restart();
-        
-    }else if (guessesLeft === 0){
+
+    } else if (guessesLeft === 0) {
         Losses++
         guessesLeft = 9;
         guessesSoFar = [];
         restart();
     }
-    
+
 
     // Display the user and computer guesses, and wins/losses/ties.
-    
+
     guessesLeftText.textContent = guessesLeft;
     WinsText.textContent = Wins;
     LossesText.textContent = Losses;
